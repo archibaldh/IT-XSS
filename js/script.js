4ceeebaf09c1a0208f1029a3b6c283dd3347f31d
Archie <archibowl@protonmail.com>
2024-05-14 22:39:27 +1000

    updating

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 let iteration;
 let n = 1;
+let phrase1 = " <br> Well done! Let's go to the next slide and try something different.";
+let phrase2 = "This is the basic idea behind XSS. Now let's look as some different versions of XSS.";
 function incrementN() {
     n++;
 }
 function input() {
-    document.getElementById(`inputDisplay${n}`).innerHTML=document.getElementById(`input${n}`).value;
+    document.getElementById(`inputDisplay${n}`).innerHTML=document.getElementById(`input${n}`).value.concat(phrase1);
 }
